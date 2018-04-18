6820152fa5fe32f72bbce81f41754b9739f8578d
xinqian <xinqian@juanpi.com>
2018-04-18 12:08:45 +0800

    优化json模块

--- src/main/java/commons/tool/utils/JsonUtil.java
+++ src/main/java/commons/tool/utils/JsonUtil.java
@@ -1,25 +1,77 @@
 package commons.tool.utils;
 
+import java.util.List;
 import java.util.Map;
 
-import org.nutz.json.Json;
-import org.nutz.json.JsonFormat;
+import com.alibaba.fastjson.JSON;
 
+/**
+ * json通用方法
+ *
+ */
 public class JsonUtil {
-    public static String toJson(Object obj) {
-        return Json.toJson(obj, JsonFormat.compact());
-    }
+	/**
+	 * obj转json字符串
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public static String toJson(Object obj) {
+		return JSON.toJSONString(obj);
+	}
 
-    public static String toJsonFull(Object obj) {
-        return Json.toJson(obj, JsonFormat.tidy());
-    }
+	/**
+	 * obj转json字符串,格式美化
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public static String toJsonFull(Object obj) {
+		return JSON.toJSONString(obj, true);
+	}
 
-    public static Object toObj(String json) {
-        if (json.startsWith("[")) {
-            return Json.fromJsonAsList(Map.class, json);
-        } else {
-            return Json.fromJson(Map.class, json);
-        }
-    }
+	/**
+	 * json字符串转obj对象
+	 * 
+	 * @param json
+	 * @param clz
+	 * @return
+	 */
+	public static <T> T toObj(String json, Class<T> clz) {
+		if (json == null) {
+			throw new RuntimeException("json is null");
+		}
+		if (json.trim().startsWith("{")) {
+			return JSON.parseObject(json, clz);
+		}
+		throw new RuntimeException("err json format");
+	}
+
+	/**
+	 * json字符串转map对象
+	 * 
+	 * @param json
+	 * @return
+	 */
+	public static Map<?, ?> toObjMap(String json) {
+		return toObj(json, Map.class);
+	}
+
+	/**
+	 * json字符串转list对象
+	 * 
+	 * @param json
+	 * @param clz
+	 * @return
+	 */
+	public static <T> List<T> toObjList(String json, Class<T> clz) {
+		if (json == null) {
+			throw new RuntimeException("json is null");
+		}
+		if (json.trim().startsWith("[")) {
+			return JSON.parseArray(json, clz);
+		}
+		throw new RuntimeException("err json format");
+	}
 
 }

6a385298286f3aaa75d8e2f49bbf99cfb7b6e107
huangjin <hjin@hb-rz.com>
2018-06-21 09:13:01 +0800

    CollectionUtil improved

--- src/main/java/commons/tool/utils/CollectionUtil.java
+++ src/main/java/commons/tool/utils/CollectionUtil.java
@@ -1,6 +1,11 @@
 package commons.tool.utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Collection;
+import java.util.List;
 
 public class CollectionUtil {
 
@@ -26,4 +31,29 @@ public class CollectionUtil {
         }
         return null;
     }
+
+
+    /**
+     * clone list
+     *
+     * @param src
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> clone(List<T> src) {
+        List<T> dest = null;
+        try {
+            ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(byteOut);
+            out.writeObject(src);
+
+            ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray());
+            ObjectInputStream in = new ObjectInputStream(byteIn);
+            dest = (List<T>) in.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dest;
+    }
+
 }
